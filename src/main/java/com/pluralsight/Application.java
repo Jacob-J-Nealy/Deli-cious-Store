ab08e2b862b47818360ac9d997fa3e1c5d137c2b
Jacob-J-Nealy <jacobnealy01@gmail.com>
2025-05-27 15:27:33 -0400

    Finished Methods in UserInterface Class for displaying and selected from Home Screen

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -1,7 +1,17 @@
 package com.pluralsight;
 
+import com.pluralsight.UserInterface;
+
+import java.util.Scanner;
+import static com.pluralsight.UserInterface.scanner;
+
+
 public class Application {
+
+    UserInterface userInterface = new UserInterface();
+    Scanner scanner1 = new Scanner(System.in);
+
     public static void main(String[] args) {
-        
+        UserInterface.uiHomeScreenSplitCase();
     }
 }
--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,4 +1,67 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class UserInterface {
+    static Scanner scanner = new Scanner(System.in);
+
+    private static boolean running;
+
+    public static void uiHomeScreenSplitCase() {
+        boolean running = true;
+
+        while (running) {
+
+            // Display HomeScreen
+            int input = UserInterface.uiHomeScreenDisplay();
+
+            switch (input) {
+
+                case 1:
+                    System.out.println("Opening New Order Menu...");
+                    break;
+                case 0:
+                    System.out.println("_____________________________________________");
+                    System.out.println("Closing Application...");
+                    running = false;
+                    break;
+                default:
+                    System.err.println("Incorrect Input. Returning to Home Screen...");
+                    break;
+            }
+
+        }
+    }
+
+    public static int uiHomeScreenDisplay() {
+
+        try {
+            int input = -1;
+
+            System.out.println("\nWelcome to DEliiii-Cious Sandwich Shop!");
+            System.out.println("'Where we put the Deli in Delicious Sandwiches!'");
+            System.out.println("_____________________________________________");
+            System.out.println("Please Choose an Option Below:\n");
+
+            System.out.println("1) *New* Order");
+            System.out.println("0) Exit Application");
+
+            System.out.print("\nInput Here: ");
+            input = scanner.nextInt();
+
+            return input;
+
+        } catch (Exception e) {
+            System.err.println("Incorrect Input. Closing Application...");
+        }
+
+        return 0;
+    }
+
+
+
+
+
+
 }
+
