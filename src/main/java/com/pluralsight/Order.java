fb5e01399597fa30df87576d38c387805dd6add9
Jacob-J-Nealy <jacobnealy01@gmail.com>
2025-05-27 21:29:38 -0400

    Added Methods & items ArrayList as a Priceable

--- src/main/java/com/pluralsight/Order.java
+++ src/main/java/com/pluralsight/Order.java
@@ -1,4 +1,16 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+
+    // Properties
+    private List<Priceable> items = new ArrayList<>();
+
+
+    // Custom Methods
+    public void addItem(Priceable item) {
+        items.add(item);
+    }
 }
