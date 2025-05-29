9de5a79f34a2e1ed83c00c3ecb92278ae42a2b7f
Jacob-J-Nealy <jacobnealy01@gmail.com>
2025-05-29 10:04:27 -0400

    Working on properties for CustomSandwich

--- src/main/java/com/pluralsight/orderItems/CustomSandwich.java
+++ src/main/java/com/pluralsight/orderItems/CustomSandwich.java
@@ -1,8 +1,13 @@
 package com.pluralsight.orderItems;
 
+import com.pluralsight.orderItems.customSandwichParts.Bread;
+
 public class CustomSandwich {
 
     //Properties
     private int size;
+    private Bread bread;
+
+
 
 }
