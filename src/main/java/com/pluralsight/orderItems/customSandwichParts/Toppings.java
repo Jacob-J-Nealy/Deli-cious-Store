29fd4e611b08a0f2e4cb1bae37a0928c6698cfc4
Jacob-J-Nealy <jacobnealy01@gmail.com>
2025-05-29 23:21:39 -0400

    Finished calculatePrice() Method inside of Meats Class

--- src/main/java/com/pluralsight/orderItems/customSandwichParts/Toppings.java
+++ src/main/java/com/pluralsight/orderItems/customSandwichParts/Toppings.java
@@ -5,15 +5,7 @@ public abstract class Toppings {
     // Toppings Properties
     private String name;
     private boolean isPremium;
-    private boolean isCheese;
-    private boolean isMeat;
 
-    public Toppings(String name, boolean isPremium, boolean isCheese, boolean isMeat) {
-        this.name = name;
-        this.isPremium = isPremium;
-        this.isCheese = isCheese;
-        this.isMeat = isMeat;
-    }
 
     // Toppings Constructor
     public Toppings(String name, boolean isPremium) {
@@ -39,15 +31,8 @@ public abstract class Toppings {
     public String getName() {
         return name;
     }
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public boolean isPremium() {
         return isPremium;
-    }
-    public void setPremium(boolean premium) {
-        isPremium = premium;
     }
 
 }
---
+++ src/main/java/com/pluralsight/orderItems/customSandwichParts/toppingsOptions/Meats.java
@@ -0,0 +1,30 @@
+package com.pluralsight.orderItems.customSandwichParts.toppingsOptions;
+
+import com.pluralsight.orderItems.customSandwichParts.Toppings;
+
+public class Meats extends Toppings {
+
+
+    // Meats Constructor
+    public Meats(String name, boolean isPremium) {
+        super(name, true);
+    }
+
+    // Fix This
+    @Override
+    public double calculatePrice(int size) {
+        if (size == 4) {
+            return 1.00;
+        }
+
+        if (size == 8) {
+            return 2.00;
+        }
+
+        if (size == 12) {
+            return 3.00;
+        }
+
+        return 0;
+    }
+}
